fc5bcf5fffddb728cbe5ace7c1ea9901ae2af71a
ral <ralthor5@gmail.com>
2019-07-14 01:29:41 +0430

    Fixed neural network calculating and learning issues

--- src/nn.js
+++ src/nn.js
@@ -1,9 +1,17 @@
 class Neuron {
-    constructor(n) {
-        var temp = Array(n).fill(0);
-        this.weigths = temp.map(i => Math.random() * 2 - 1);
-        this.bias =  temp.map(i => Math.random() * 2 - 1);
+    constructor(n, firstLayer = false, useSigmoid = true) {
+        if (firstLayer) {
+            this.weigths = Array(n).fill(1);
+            this.bias = Array(n).fill(1);
+        }
+        else {
+            var temp = Array(n).fill(0);
+            this.weigths = temp.map(i => Math.random() * 2 - 1);
+            this.bias = temp.map(i => Math.random() * 2 - 1);
+        }
         this.n = n;
+        this.firstLayer = firstLayer;
+        this.useSigmoid = useSigmoid;
     }
 
     output(a) {
@@ -11,6 +19,8 @@ class Neuron {
     }
 
     sigmoid(t) {
+        if (this.firstLayer || !this.useSigmoid)
+            return t;
         return 1 / (1 + Math.exp(-t));
     }
 
@@ -20,20 +30,23 @@ class Neuron {
 
     fromArray(array) {
         var n = array.length;
-        this.weigths = array.slice(0, n/2)
-        this.bias = array.slice(n/2)
+        this.weigths = array.slice(0, n / 2)
+        this.bias = array.slice(n / 2)
     }
 }
 
 class NeuralNetworkLayer {
-    constructor(n, numberOfPreviousLayerNeurons, firstLayer = false) {
-        this.neurons = Array(n).fill(0).map(i => new Neuron(firstLayer ? 1: numberOfPreviousLayerNeurons));
+    constructor(n, numberOfPreviousLayerNeurons, firstLayer = false, useSigmoid = true) {
+        this.neurons = Array(n).fill(0).map(i => new Neuron(firstLayer ? 1 : numberOfPreviousLayerNeurons, 
+            firstLayer, useSigmoid));
         this.firstLayer = firstLayer;
         this.size = n * this.neurons[0].n;
     }
 
-    output (a) {
-        if(this.firstLayer)
+    output(a) {
+        if(!Array.isArray(a))
+            a = [a];
+        if (this.firstLayer)
             return this.neurons.map((x, i) => x.output([a[i]]));
         else
             return this.neurons.map(x => x.output(a));
@@ -46,7 +59,7 @@ class NeuralNetworkLayer {
     fromArray(array) {
         var n = array.length;
         var m = this.neurons[0].n * 2;
-        for(var i = 0; i < n / m; i++)
+        for (var i = 0; i < n / m; i++)
             this.neurons[i].fromArray(array.slice(i * m, (i + 1) * m))
     }
 }
@@ -54,11 +67,11 @@ class NeuralNetworkLayer {
 class NeuralNetwork {
     constructor(layers) {
         this.layers = Array(layers.length).fill(0).map(
-            (_, i) => new NeuralNetworkLayer(layers[i], i != 0 ? layers[i - 1]: 1, i == 0));
+            (_, i) => new NeuralNetworkLayer(layers[i], i != 0 ? layers[i - 1] : 1, i == 0, i != layers.length - 1));
     }
 
     output(a) {
-        for(var i = 0; i < this.layers.length; i++)
+        for (var i = 0; i < this.layers.length; i++)
             a = this.layers[i].output(a);
         return a;
     }
@@ -68,7 +81,7 @@ class NeuralNetwork {
     }
 
     fromArray(array) {
-        for(var i = 0, j = 0; i < this.layers.length; i++) {
+        for (var i = 0, j = 0; i < this.layers.length; i++) {
             var nextJ = j + this.layers[i].size * 2;
             this.layers[i].fromArray(array.slice(j, nextJ));
             j = nextJ;
